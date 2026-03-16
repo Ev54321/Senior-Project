d87f1cd52c72cfed649e3757a55fc0ddfbcce387
Ev54321 <163931274+Ev54321@users.noreply.github.com>
2026-03-16 13:51:09 -0700

    Adding images and content to history

--- parallax-scrolling.js
+++
@@ -1,7 +0,0 @@
-var image = document.getElementsByClassName("background");
-new simpleParallax(image, {
-    orientation: 'down',
-    scale: 1.5,
-    delay: .6,
-    transition: 'cubic-bezier(0,0,0,1)'
-});
