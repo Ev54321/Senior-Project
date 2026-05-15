0394c9e2b268b42d71d8cf58f1ca0454900b9841
Ev54321 <163931274+Ev54321@users.noreply.github.com>
2026-05-15 14:00:56 -0700

    Adding images and fixing page layout

--- video.js
+++ video.js
@@ -1,7 +1,7 @@
 const video = document.createElement('video');
 video.src = 'video/Century-vs-OES.mp4';
 video.controls = true;
-video.width = 450;
-video.height = 400;
+video.width = 500;
+video.height = 300;
 
 document.getElementById('video-container').appendChild(video);
